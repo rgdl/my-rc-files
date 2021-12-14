54aff9f165fe2666427ec6d73853b85d08ad0336
rdelisle-godaddy <rdelisle@godaddy.com>
2021-12-14 10:40:26 +1000

    put warning in setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+echo
+read -p "WARNING: this script will download vim-plug and overwrite existing rc files. Do you want to continue? (y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+
 # Download vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
