a5bb21c2210342c7de8044632119315883e8cb3f
rgdl <robert_delisle44@hotmail.com>
2022-05-10 10:35:07 +1000

    add flag to skip y/n confirmation

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
 
-echo
-read -p "WARNING: this script will download vim-plug and overwrite existing rc files. Do you want to continue? (y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+if [[ $* != *-y* ]]
+then
+    echo
+    read -p "WARNING: this script will download vim-plug and overwrite existing rc files. Do you want to continue? (y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+fi
 
 # Download vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
